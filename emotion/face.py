351cdbaf554abc165d06d3c321339ec352025d97
hiroshima <983713+hiroshungo@users.noreply.github.com>
2024-10-01 21:59:46 +0900

    変更その他

---
+++ emotion/face.py
@@ -0,0 +1,11 @@
+# pythonライブラリのFERを用いる
+from fer import FER
+import matplotlib.pyplot as plt
+
+# 感情分析に使う画像を読み込む
+test_image_one = plt.imread("face.jpg")
+
+emo_detector = FER(mtcnn=True)
+dominant_emotion, emotion_score = emo_detector.top_emotion(test_image_one)
+plt.imshow(test_image_one)
+print(dominant_emotion, emotion_score)
