f9217d607207c957d16af12f85606a552d460a53
hiroshima <983713+hiroshungo@users.noreply.github.com>
2024-10-02 01:28:05 +0900

    パス変更

--- emotion/face.py
+++ emotion/face.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas
 
 # 感情分析に使う画像を読み込む
-test_image_one = plt.imread("face.jpg")
+test_image_one = plt.imread("emotion\\face.jpg")
 
 emo_detector = FER(mtcnn=True)
 dominant_emotion, emotion_score = emo_detector.top_emotion(test_image_one)
